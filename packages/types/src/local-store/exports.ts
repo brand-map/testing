d8dfc2a3ee94f3e81c6b27c1b9d45002cfc45247
Alexey Sokolov <alexey@brand-map.online>
2026-01-13 14:08:05 +0300

    adjust types

--- packages/types/src/local-store/exports.ts
+++ packages/types/src/local-store/exports.ts
@@ -3,5 +3,5 @@ import type { Result } from "neverthrow";
 export interface LocalStoreInterface {
   set(key: string, data: unknown): Promise<Result<void, Error>>;
   get<T>(key: string): Promise<Result<T | null, Error>>;
-  remove(key: string): void;
+  remove(key: string): Promise<void>;
 }
